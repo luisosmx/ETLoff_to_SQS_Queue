4851d1928b11a8ddddb0d8421b3468be05a0c6e2
luisosmx <119461863+luisosmx@users.noreply.github.com>
2023-10-13 11:29:54 -0600

    add script test

--- read-json.py
+++ read-json.py
@@ -1,21 +1,16 @@
 import pandas as pd
 import boto3
 import json
-import psycopg2
 from sqlalchemy import create_engine
 
-# Create an SQS client using the localstack endpoint
 sqs = boto3.client('sqs', endpoint_url='http://localhost:4566')
 
-# Specify the URL of the queue you want to receive messages from
 queue_url = 'http://localhost:4566/000000000000/login-queue'
 
-# Number of messages to receive at a time
 max_messages = 1
 user_login = []
 
 while True:
-    # Receive messages from the queue
     response = sqs.receive_message(
         QueueUrl=queue_url,
         AttributeNames=['All'],
@@ -48,7 +43,7 @@ df = pd.DataFrame(masked_data)
 
 
 db_config = {
-    "host": "localhost",  # Cambia esto al host de tu servidor PostgreSQL
+    "host": "localhost",  
     "database": "postgres",
     "user": "postgres",
     "password": "postgres"
@@ -56,10 +51,8 @@ db_config = {
 
 engine = create_engine(f'postgresql+psycopg2://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}/{db_config["database"]}')
 
-# Guardar el DataFrame en la tabla "user_logins"
 df.to_sql("user_logins", engine, if_exists="replace", index=False)
 
-# Cerrar la conexión
 engine.dispose()
 
 
