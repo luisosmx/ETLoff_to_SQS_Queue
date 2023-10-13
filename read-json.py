4088f133ec6581811cea0b3651be021064f41a3b
luisosmx <119461863+luisosmx@users.noreply.github.com>
2023-10-13 11:28:28 -0600

    add script dummy

--- read-json.py
+++ read-json.py
@@ -1,7 +1,8 @@
+import pandas as pd
 import boto3
-import time
 import json
-
+import psycopg2
+from sqlalchemy import create_engine
 
 # Create an SQS client using the localstack endpoint
 sqs = boto3.client('sqs', endpoint_url='http://localhost:4566')
@@ -12,50 +13,53 @@ queue_url = 'http://localhost:4566/000000000000/login-queue'
 # Number of messages to receive at a time
 max_messages = 1
 user_login = []
-state = True
 
 while True:
     # Receive messages from the queue
     response = sqs.receive_message(
         QueueUrl=queue_url,
-        AttributeNames=[
-            'All'
-        ],
+        AttributeNames=['All'],
         MaxNumberOfMessages=max_messages,
-        MessageAttributeNames=[
-            'All'
-        ],
+        MessageAttributeNames=['All'],
         VisibilityTimeout=0,
         WaitTimeSeconds=0
     )
-    # Check if there are messages in the response
+
     if 'Messages' in response:
         for message in response['Messages']:
-            # Process the message
-            #print(f"Received message: {message['Body']}")
             user_login.append(message['Body'])
 
             sqs.delete_message(
                 QueueUrl=queue_url,
                 ReceiptHandle=message['ReceiptHandle']
             )
-            
     else:
         break
-        #print("No messages in the queue. Waiting for messages...")
-        #time.sleep(2)
+
 masked_data = []
 
 for item in user_login:
     item_dict = json.loads(item)
-
     item_dict['device_id'] = '*** Masked ***'
     item_dict['ip'] = '*** Masked ***'
+    masked_data.append(item_dict)
 
-    masked_item = json.dumps(item_dict)
-
-    masked_data.append(masked_item)
+df = pd.DataFrame(masked_data)
 
 
-print(masked_data)
+db_config = {
+    "host": "localhost",  # Cambia esto al host de tu servidor PostgreSQL
+    "database": "postgres",
+    "user": "postgres",
+    "password": "postgres"
+}
+
+engine = create_engine(f'postgresql+psycopg2://{db_config["user"]}:{db_config["password"]}@{db_config["host"]}/{db_config["database"]}')
+
+# Guardar el DataFrame en la tabla "user_logins"
+df.to_sql("user_logins", engine, if_exists="replace", index=False)
+
+# Cerrar la conexión
+engine.dispose()
+
 
